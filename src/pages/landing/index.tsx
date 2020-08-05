efb6e0f3c8ed0a1e7c89ad3b94be0ccc80773149
Daniel Corrêa <eldestemido@gmail.com>
2020-08-05 18:11:12 -0300

    page index ready

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -13,32 +13,31 @@ import giveClassesIcon from '../../assets/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
 
 function Landing() {
-    return(
-      <div id="page-landing">
-          <div id="page-landing-content" className="container">
-              <div className="logo-container">
-                  <img src={LogoImg} alt="proffy-brand"/>
-                  <h2>Sua Plataforma de estudos online.</h2>
-              </div>
-              <img src={LandImg} alt="proffy-landing" className="hero-image"/>
-              <div className="buttons-container">
-                  <a href="http://localhost" className="study">
-                      <img src={studyIcon} alt="study proffy app"/>
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={LogoImg} alt="proffy-brand" />
+                    <h2>Sua Plataforma de estudos online.</h2>
+                </div>
+                <img src={LandImg} alt="proffy-landing" className="hero-image" />
+                <div className="buttons-container">
+                    <a href="http://localhost" className="study">
+                        <img src={studyIcon} alt="study proffy app" />
                       Estudar
-                  </a>
+                    </a>
 
-                  <a href="http://localhost" className="give-classes">
-                      <img src={giveClassesIcon} alt="give classes"/>
-                      Estudar
-                  </a>
-
-                  <span className="total-connections">
-                      Total de 2000 connexões já realizadas
-                      <img src={purpleHeartIcon} alt="total connections"/>
-                  </span>
-              </div>
-          </div>
-      </div>
+                    <a href="http://localhost" className="give-classes">
+                        <img src={giveClassesIcon} alt="give classes" />
+                      Dar Aulas
+                    </a>
+                </div>
+                <span className="total-connections">
+                    Total de 2000 connexões já realizadas
+                      <img src={purpleHeartIcon} alt="total connections" />
+                </span>
+            </div>
+        </div>
     );
 }
 
